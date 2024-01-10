137b0e48b560f5aaa5996283bfce67f087e4cea5
Minta-Darkwah <ohenebaminta.darkwah@istitutoagnelli.it>
2024-01-10 08:33:14 +0100

    sito web

--- todos-web/svelte.config.js
+++ todos-web/svelte.config.js
@@ -4,8 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-            pages: 'build-static',
-            assets: 'build-static',
+            pages: 'docs',
+            assets: 'docs',
             fallback: undefined,
             precompress: false,
             strict: true
